66034f53e910fe531a8f3e7b8fea9327956ce4ab
Cinix03 <vasileirinel013@gmail.com>
2025-11-14 14:53:36 +0200

    add cors

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"time"
+
 	"github.com/SerbanEduard/ProiectColectivBackEnd/config"
 	"github.com/SerbanEduard/ProiectColectivBackEnd/docs"
 	"github.com/SerbanEduard/ProiectColectivBackEnd/routes"
+	"github.com/gin-contrib/cors"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
@@ -19,6 +22,15 @@ func main() {
 	config.InitFirebase()
 
 	r := routes.SetupRoutes()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://studyflow-6qwx.onrender.co"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	// Setup Swagger
 	docs.SwaggerInfo.BasePath = "/"
